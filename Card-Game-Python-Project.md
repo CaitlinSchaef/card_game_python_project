8002b72d53a7e001b0df401e9ff47dd2cf876c53
Caitlin Schaeffer <caitlinschaef@gmail.com>
2024-05-08 15:25:40 -0400

    can you commit from the environment

---
+++ Card-Game-Python-Project.md
@@ -0,0 +1,16 @@
+## Logic:
+## Cards:
+Make two variables that have lists assigned to them, one being value and one being suits
+Then to make cards add each element of the value array to each of the suits
+Those are your cards that make up the deck
+
+## Deck:
+Holds the cards, basically I think you just need to assign it a numeric value, so that you can say if you want to play with 1 or 2 decks (or more)
+Should the shuffle function be in this class?
+
+## Players
+Signifies the number of players
+Default 2 players, one is the computer/terminal and the other is the user
+So I guess in theory you could name them: 'Dealer' & 'Player' 
+OR if you didn't want the temrinal to play you could assign 'Player 1' & 'Player 2' and have people play against eachother
+If Dealer + Player, assign an input to Player value for the terminal to take someone's name
--- my_script.py
+++ my_script.py
@@ -9,35 +9,45 @@ class Card:
         'Spades',
         'Clubs'
     ]
-    # cards start at 2, not 0, so making the indexes line up 
+    # cards start at 2, not 0, so making the indexes line up, but Ace can be 0 or 1
     values = [
-        None, None, '2', '3', '4', '5', '6', '7', '8', '9', '10', 'Jack', 'Queen', 'King', 'Ace'
+        None, None , 2, 3, 4, 5, 6, 7, 8, 9, 10, 'Jack', 'Queen', 'King', 'Ace'
     ]
 
+    # set integer values for face cards
+    # face_cards = {
+    #     'Jack': 11,
+    #     'Queen': 12,
+    #     'King': 13,
+    #     'Ace': 14
+    # }
+
     # this initializes the variables 
-    def __init__(self, suit, values):
+    def __init__(self, suit=suit, values=values):
         self.suit = suit
         self.values = values
-    
+    print(values)
+    print(suit)
+    list_of_cards = []
     # lets make a produce card method -this is legit broken
-    def cards(self, diamonds, values):
-        if (self.suit == "Diamond"):
-          diamonds = map(self.values, + "of Diamonds")
-          return diamonds
-        elif (self.suit == "Hearts"):
-            hearts = self.values + "of Hearts"
-            return hearts
-        elif (self.suit == "Spades"):
-            spades = self.values + "of Spades"
-            return spades
-        elif (self.suit == "Clubs"):
-            clubs = self.values + "of Clubs"
-            return clubs
-        else:
-            return "No card here!"
-        
+    # def make_cards(self):
+    #     self.suit[0] = diamonds
+    #     diamonds = self.values.slice('') + "of Diamonds"
+# this_is_the_card_i_am_creating_it_is_a_new_and_unique_snowflake = Card('heart', 9)
 
+# print(f'{this_is_the_card_i_am_creating_it_is_a_new_and_unique_snowflake.suit} {this_is_the_card_i_am_creating_it_is_a_new_and_unique_snowflake.values}')
+# #gives us heart
+    
+class Deck(Card):
+    suits = []
+    values = []
+    list_of_cards = []
+    for suit in suits:
+        for value in values:
+            list_of_cards.append(Card(suit, value))
+    for card in list_of_cards:
+        print(f'card: {card.suit}{card.number}')
+        print('\n')
 
-# class Deck:
 
 # class Player:
