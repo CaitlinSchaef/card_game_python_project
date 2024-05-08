4cdb72fbd0f7d68a19797dd71601fcedfbeb8e08
Caitlin Schaeffer <caitlinschaef@gmail.com>
2024-05-08 16:48:53 -0400

    you are producing a deck at least

--- my_script.py
+++ my_script.py
@@ -1,34 +1,26 @@
 # python my_script.py
-
-class Card: 
+# this is A card, a singular card
 # build a dictionary of suits and values
 # python 'none' = js 'null'
-    suit = [
-        'Diamonds',
-        'Hearts', 
-        'Spades',
-        'Clubs'
-    ]
-    # cards start at 2, not 0, so making the indexes line up, but Ace can be 0 or 1
-    values = [
-        None, None , 2, 3, 4, 5, 6, 7, 8, 9, 10, 'Jack', 'Queen', 'King', 'Ace'
-    ]
-
-    # set integer values for face cards
-    # face_cards = {
-    #     'Jack': 11,
-    #     'Queen': 12,
-    #     'King': 13,
-    #     'Ace': 14
-    # }
-
+suits = [
+    'Diamonds',
+    'Hearts', 
+    'Spades',
+    'Clubs'
+]
+# cards start at 2, not 0, so making the indexes line up, but Ace can be 0 or 1
+values = [
+    2, 3, 4, 5, 6, 7, 8, 9, 10, 'Jack', 'Queen', 'King', 'Ace'
+]
+class Card: 
     # this initializes the variables 
-    def __init__(self, suit=suit, values=values):
+    def __init__(self, suit, value):
         self.suit = suit
-        self.values = values
-    print(values)
-    print(suit)
-    list_of_cards = []
+        self.value = value
+        # print(self.value)
+        # print(self.suit)
+    
+    
     # lets make a produce card method -this is legit broken
     # def make_cards(self):
     #     self.suit[0] = diamonds
@@ -37,17 +29,28 @@ class Card:
 
 # print(f'{this_is_the_card_i_am_creating_it_is_a_new_and_unique_snowflake.suit} {this_is_the_card_i_am_creating_it_is_a_new_and_unique_snowflake.values}')
 # #gives us heart
-    
-class Deck(Card):
-    suits = []
-    values = []
-    list_of_cards = []
-    for suit in suits:
-        for value in values:
-            list_of_cards.append(Card(suit, value))
-    for card in list_of_cards:
-        print(f'card: {card.suit}{card.number}')
-        print('\n')
+
+#class is a special function that makes objects, and automatically returns a new one, the init part is the constructor that says what happens when we make a new one 
+class Deck:
+    def __init__(self):
+        self.list_of_cards = [Card(suit, value) for suit in suits for value in values]
+#new deck has a property in it called list of cards, so new_deck.list_of_cards is the array 
+new_deck = Deck()
+for card in new_deck.list_of_cards: 
+    # this is just a loop to look at them, and it does produce all of the lines
+    print(card.value, 'of'  ,card.suit)
+
+    def shuffle(self):
+
+
 
 
 # class Player:
+# player = input('What's your name?)
+
+#class Game: 
+# weight the cards value
+
+#declare winner 
+#class Dealer:
+#dealer gets a card
